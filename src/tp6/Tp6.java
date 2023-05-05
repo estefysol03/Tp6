266903ea9ea54f06d412941de34dd338b6e68f92
estefysol03 <estefysol03@gmail.com>
2023-05-05 09:14:36 -0300

    editado

--- src/tp6/Tp6.java
+++ src/tp6/Tp6.java
@@ -13,7 +13,7 @@ public class Tp6 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {  
         // TODO code application logic here
     }
     
